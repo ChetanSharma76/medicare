12d8aabd1d78740d4b5dd30c28001718994851f0
ChetanSharma76 <159418211+ChetanSharma76@users.noreply.github.com>
2025-07-11 22:23:19 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -37,10 +37,6 @@ const Navbar = () => {
                 <li className='py-1'>CONTACT</li>
                 <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
             </NavLink>
-            <NavLink to="http://localhost:5174/">
-                <li className="w-full py-1 font-extrabold bg-gradient-to-r from-blue-500 via-green-400 to-purple-500 text-transparent bg-clip-text">ADMIN PANEL</li>
-                <hr className='border-none outline-none h-0.5 bg-primary w-3/5 m-auto hidden' />
-            </NavLink>
         </ul>
         <div className='flex items-center gap-4'>
             {   
