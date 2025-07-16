d70265c2f6a84fd093f6fe2a34440259b38ea677
ChetanSharma76 <159418211+ChetanSharma76@users.noreply.github.com>
2025-07-16 18:40:40 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
     try {
       
       if(state==='Sign Up'){
-        const data = await axios.post(backendUrl+'/api/user/register',{name,email,password});
+        const {data} = await axios.post(backendUrl+'/api/user/register',{name,email,password});
         console.log(data)
         if(data.success){
           toast.success('Account Created Successfully')
